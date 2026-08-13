ee1c7808e4fb443ef492b915d4ff2b69c1a73463
Darayus Giromaar <tuv89272@nd05.mol.ax>
2026-08-13 17:09:10 +0000

    population growth rate validation

---
+++ _pre_calibration_population_growth_rate_validation/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /work/tuv89272/Calibration_Pipeline_v7_NG-SE/_pre_calibration_population_growth_rate_validation || exit 1
+
+nohup ./bin/MalaSim \
+-i input/input_population_bins_beta_zero_pop_0.001.yml \
+-r SQLiteMonthlyReporter \
+-o output/pop_validation \
+-j 0 \
+-v 1 \
+> 0.log 2>&1 &
+
+echo $! > malaSim_0.pid
+echo "MalaSim started in background"
+echo "PID: $(cat malaSim_0.pid)"
+echo "Log: 0.log"
